8132b4ab2b1dfd86f4e43be43f499889fa7acddd
Marcin <marcin.lisiecki@tutanota.com>
2021-06-03 16:59:52 +0200

    basic drawer with labels

--- drawer.py
+++ drawer.py
@@ -1,4 +1,3 @@
-
 from graphviz import Graph
 
 from code_parser import Parser
@@ -6,19 +5,24 @@ from code_parser import Parser
 
 class Drawer:
     def configure_graph(self) -> Graph:
-        graph = Graph("ER", filename='output.gv', engine='neato')
-        graph.attr('node', shape='box')
+        graph = Graph("G", filename='output.gv', engine='dot')
+        graph.attr('node', shape='box', fixed_size='true')
         return graph
 
-    def create_nodes(self, labels_dict) -> None:
+    def create_nodes_with_edges(self, list_of_mnemo_obj) -> None:
         graph = self.configure_graph()
-        for label in labels_dict.keys():
-            graph.node(label)
+        for element in list_of_mnemo_obj:
+            graph.node(element.root)
+            graph.node(element.target_label)
+        for element in list_of_mnemo_obj:
+            graph.edge(element.root, element.target_label, label=element.name, dir='forward')
         graph.view()
 
 
 if __name__ == '__main__':
     prs = Parser('test_file.asm')
-    labels = prs.find_labels()
+    labels_dict = prs.find_labels()
+    labels_with_code_dict = prs.find_code_under_labels(labels_dict)
+    list_of_mnemo_obj = prs.find_mnemonics(labels_with_code_dict)
     drw = Drawer()
-    drw.create_nodes(labels)
+    drw.create_nodes_with_edges(list_of_mnemo_obj)
